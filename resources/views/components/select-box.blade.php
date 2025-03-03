3021214534a6dbc3e7dcc84243d7a51a1ba15c1e
mahdi zangeneh <67897876+bigmemti@users.noreply.github.com>
2025-03-03 23:20:28 +0330

    add require attribute to input components

--- resources/views/components/select-box.blade.php
+++ resources/views/components/select-box.blade.php
@@ -1,6 +1,6 @@
-@props(['disabled' => false])
+@props(['disabled' => false, 'required' => false])
 
-<select  @disabled($disabled) {{ $attributes->merge(['class' => 'border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-emerald-500 dark:focus:border-emerald-600 focus:ring-emerald-500 dark:focus:ring-emerald-600 rounded-md shadow-sm']) }}>
-    <option>{{ __('Plase select') }}</option>
+<select  @disabled($disabled) @required($required) {{ $attributes->merge(['class' => 'border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-emerald-500 dark:focus:border-emerald-600 focus:ring-emerald-500 dark:focus:ring-emerald-600 rounded-md shadow-sm']) }}>
+    <option value="">{{ __('Plase select') }}</option>
     {{ $slot }}
 </select>
